3609c979c33c1db28d48d1d9155d5ab7e1df36d1
tristanpendergrass <tristanpendergrass@gmail.com>
2023-02-13 19:43:28 -0800

    Speed up getStats for chores and shopitems by refactoring the stats into top level consts

--- src/IdleGame/Chore.elm
+++ src/IdleGame/Chore.elm
@@ -65,140 +65,176 @@ incrementChoreMxp amount kind choresData =
     stats.setter (\choreData -> { choreData | mxp = choreData.mxp + amount }) choresData
 
 
+cleanStablesStats =
+    { kind = CleanStables
+    , title = "Clean Stables"
+    , imgSrc = "/chores/stable.png"
+    , getter = .cleanStables
+    , setter = \fn choresData -> { choresData | cleanStables = fn choresData.cleanStables }
+    , outcome =
+        { xp = 10
+        , duration = 3000
+        , extraResourceProbability = 0.25
+        , extraResource = Resource.Manure
+        , coin = Coin.create 1
+        }
+    }
+
+
+cleanBigBubbaStats =
+    { kind = CleanBigBubba
+    , title = "Clean Big Bubba's Stall"
+    , imgSrc = "/chores/bubba4.png"
+    , getter = .cleanBigBubba
+    , setter = \fn choresData -> { choresData | cleanBigBubba = fn choresData.cleanBigBubba }
+    , outcome =
+        { xp = 25
+        , duration = 6000
+        , extraResourceProbability = 0.75
+        , extraResource = Resource.Manure
+        , coin = Coin.create 3
+        }
+    }
+
+
+sweepChimneysStats =
+    { kind = SweepChimneys
+    , title = "Sweep Chimneys"
+    , imgSrc = "/chores/chimney.png"
+    , getter = .sweepChimneys
+    , setter = \fn choresData -> { choresData | sweepChimneys = fn choresData.sweepChimneys }
+    , outcome =
+        { xp = 37
+        , duration = 8000
+        , extraResourceProbability = 0.5
+        , extraResource = Resource.Soot
+        , coin = Coin.create 6
+        }
+    }
+
+
+waterGreenhousePlantsStats =
+    { kind = WaterGreenhousePlants
+    , title = "Water Greenhouse Plants"
+    , imgSrc = "/chores/greenhouse_3.png"
+    , getter = .waterGreenhousePlants
+    , setter = \fn choresData -> { choresData | waterGreenhousePlants = fn choresData.waterGreenhousePlants }
+    , outcome =
+        { xp = 12
+        , duration = 2000
+        , extraResourceProbability = 0.6
+        , extraResource = Resource.GreenhouseDirt
+        , coin = Coin.create 2
+        }
+    }
+
+
+washAndIronRobesStats =
+    { kind = WashAndIronRobes
+    , title = "Wash and Iron Robes"
+    , imgSrc = "/chores/washRobes.png"
+    , getter = .washRobes
+    , setter = \fn choresData -> { choresData | washRobes = fn choresData.washRobes }
+    , outcome =
+        { xp = 50
+        , duration = 8000
+        , extraResourceProbability = 0.2
+        , extraResource = Resource.WashWater
+        , coin = Coin.create 9
+        }
+    }
+
+
+organizePotionIngredientsStats =
+    { kind = OrganizePotionIngredients
+    , title = "Organize Potion Ingredients"
+    , imgSrc = "/chores/potionIngredients_2.png"
+    , getter = .organizePotionIngredients
+    , setter = \fn choresData -> { choresData | organizePotionIngredients = fn choresData.organizePotionIngredients }
+    , outcome =
+        { xp = 165
+        , duration = 20000
+        , extraResourceProbability = 0.1
+        , extraResource = Resource.EmptyBottle
+        , coin = Coin.create 12
+        }
+    }
+
+
+repairInstrumentsStats =
+    { kind = RepairInstruments
+    , title = "Repair Instruments"
+    , imgSrc = "/chores/repairInstruments.png"
+    , getter = .repairInstruments
+    , setter = \fn choresData -> { choresData | repairInstruments = fn choresData.repairInstruments }
+    , outcome =
+        { xp = 75
+        , duration = 12000
+        , extraResourceProbability = 0.25
+        , extraResource = Resource.Scrap
+        , coin = Coin.create 20
+        }
+    }
+
+
+flushDrainDemonsStats =
+    { kind = FlushDrainDemons
+    , title = "Flush the Drain Demons"
+    , imgSrc = "/chores/drainDemons.png"
+    , getter = .flushDrainDemons
+    , setter = \fn choresData -> { choresData | flushDrainDemons = fn choresData.flushDrainDemons }
+    , outcome =
+        { xp = 90
+        , duration = 10000
+        , extraResourceProbability = 0.5
+        , extraResource = Resource.Ectoplasm
+        , coin = Coin.create 14
+        }
+    }
+
+
+organizeSpellBooksStats =
+    { kind = OrganizeSpellBooks
+    , title = "Organize Spell Books"
+    , imgSrc = "/chores/spellBooks.png"
+    , getter = .organizeSpellBooks
+    , setter = \fn choresData -> { choresData | organizeSpellBooks = fn choresData.organizeSpellBooks }
+    , outcome =
+        { xp = 210
+        , duration = 20000
+        , extraResourceProbability = 0.1
+        , extraResource = Resource.Parchment
+        , coin = Coin.create 30
+        }
+    }
+
+
 getStats : ChoreKind -> Stats
 getStats kind =
     case kind of
         CleanStables ->
-            { kind = CleanStables
-            , title = "Clean Stables"
-            , imgSrc = "/chores/stable.png"
-            , getter = .cleanStables
-            , setter = \fn choresData -> { choresData | cleanStables = fn choresData.cleanStables }
-            , outcome =
-                { xp = 10
-                , duration = 3000
-                , extraResourceProbability = 0.25
-                , extraResource = Resource.Manure
-                , coin = Coin.create 1
-                }
-            }
+            cleanStablesStats
 
         CleanBigBubba ->
-            { kind = CleanBigBubba
-            , title = "Clean Big Bubba's Stall"
-            , imgSrc = "/chores/bubba4.png"
-            , getter = .cleanBigBubba
-            , setter = \fn choresData -> { choresData | cleanBigBubba = fn choresData.cleanBigBubba }
-            , outcome =
-                { xp = 25
-                , duration = 6000
-                , extraResourceProbability = 0.75
-                , extraResource = Resource.Manure
-                , coin = Coin.create 3
-                }
-            }
+            cleanBigBubbaStats
 
         SweepChimneys ->
-            { kind = SweepChimneys
-            , title = "Sweep Chimneys"
-            , imgSrc = "/chores/chimney.png"
-            , getter = .sweepChimneys
-            , setter = \fn choresData -> { choresData | sweepChimneys = fn choresData.sweepChimneys }
-            , outcome =
-                { xp = 37
-                , duration = 8000
-                , extraResourceProbability = 0.5
-                , extraResource = Resource.Soot
-                , coin = Coin.create 6
-                }
-            }
+            sweepChimneysStats
 
         WaterGreenhousePlants ->
-            { kind = WaterGreenhousePlants
-            , title = "Water Greenhouse Plants"
-            , imgSrc = "/chores/greenhouse_3.png"
-            , getter = .waterGreenhousePlants
-            , setter = \fn choresData -> { choresData | waterGreenhousePlants = fn choresData.waterGreenhousePlants }
-            , outcome =
-                { xp = 12
-                , duration = 2000
-                , extraResourceProbability = 0.6
-                , extraResource = Resource.GreenhouseDirt
-                , coin = Coin.create 2
-                }
-            }
+            waterGreenhousePlantsStats
 
         WashAndIronRobes ->
-            { kind = WashAndIronRobes
-            , title = "Wash and Iron Robes"
-            , imgSrc = "/chores/washRobes.png"
-            , getter = .washRobes
-            , setter = \fn choresData -> { choresData | washRobes = fn choresData.washRobes }
-            , outcome =
-                { xp = 50
-                , duration = 8000
-                , extraResourceProbability = 0.2
-                , extraResource = Resource.WashWater
-                , coin = Coin.create 9
-                }
-            }
+            washAndIronRobesStats
 
         OrganizePotionIngredients ->
-            { kind = OrganizePotionIngredients
-            , title = "Organize Potion Ingredients"
-            , imgSrc = "/chores/potionIngredients_2.png"
-            , getter = .organizePotionIngredients
-            , setter = \fn choresData -> { choresData | organizePotionIngredients = fn choresData.organizePotionIngredients }
-            , outcome =
-                { xp = 165
-                , duration = 20000
-                , extraResourceProbability = 0.1
-                , extraResource = Resource.EmptyBottle
-                , coin = Coin.create 12
-                }
-            }
+            organizePotionIngredientsStats
 
         RepairInstruments ->
-            { kind = RepairInstruments
-            , title = "Repair Instruments"
-            , imgSrc = "/chores/repairInstruments.png"
-            , getter = .repairInstruments
-            , setter = \fn choresData -> { choresData | repairInstruments = fn choresData.repairInstruments }
-            , outcome =
-                { xp = 75
-                , duration = 12000
-                , extraResourceProbability = 0.25
-                , extraResource = Resource.Scrap
-                , coin = Coin.create 20
-                }
-            }
+            repairInstrumentsStats
 
         FlushDrainDemons ->
-            { kind = FlushDrainDemons
-            , title = "Flush the Drain Demons"
-            , imgSrc = "/chores/drainDemons.png"
-            , getter = .flushDrainDemons
-            , setter = \fn choresData -> { choresData | flushDrainDemons = fn choresData.flushDrainDemons }
-            , outcome =
-                { xp = 90
-                , duration = 10000
-                , extraResourceProbability = 0.5
-                , extraResource = Resource.Ectoplasm
-                , coin = Coin.create 14
-                }
-            }
+            flushDrainDemonsStats
 
         OrganizeSpellBooks ->
-            { kind = OrganizeSpellBooks
-            , title = "Organize Spell Books"
-            , imgSrc = "/chores/spellBooks.png"
-            , getter = .organizeSpellBooks
-            , setter = \fn choresData -> { choresData | organizeSpellBooks = fn choresData.organizeSpellBooks }
-            , outcome =
-                { xp = 210
-                , duration = 20000
-                , extraResourceProbability = 0.1
-                , extraResource = Resource.Parchment
-                , coin = Coin.create 30
-                }
-            }
+            organizeSpellBooksStats
