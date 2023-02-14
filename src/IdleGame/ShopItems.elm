eaf9d9aeb16e89b6e23dc8f725d8683e62f5376a
tristanpendergrass <tristanpendergrass@gmail.com>
2023-02-13 22:43:49 -0800

    Revert "Speed up getStats for chores and shopitems by refactoring the stats into top level consts"
    This reverts commit 3609c979c33c1db28d48d1d9155d5ab7e1df36d1.

--- src/IdleGame/ShopItems.elm
+++ src/IdleGame/ShopItems.elm
@@ -39,107 +39,87 @@ dummyReward =
     ShopItemIntervalMod [ { kind = CleanStables, percentChange = 0.1 } ]
 
 
-shovelStats =
-    { title = "Shovel"
-    , icon = Icon.shovel
-    , price = Coin.create 50
-    , unlockLevel = 1
-    , reward =
-        ShopItemIntervalMod
-            [ { kind = CleanStables, percentChange = 0.05 }
-            , { kind = CleanBigBubba, percentChange = 0.05 }
-            ]
-    , description = "+5% faster at Clean Stables and Clean Big Bubba's Stall"
-    , getter = .shovel
-    , setter = \owned ownedItems -> { ownedItems | shovel = owned }
-    }
-
-
-beginnerDualWieldingStats =
-    { title = "Beginner Dual Wielding"
-    , icon = Icon.book
-    , price = Coin.create 6000
-    , unlockLevel = 35
-    , reward = ShopItemIntervalMod [ { kind = WaterGreenhousePlants, percentChange = 1.0 } ]
-    , description = "+100% faster at Water Greenhouse Plants"
-    , getter = .beginnerDualWielding
-    , setter = \owned ownedItems -> { ownedItems | beginnerDualWielding = owned }
-    }
-
-
-keyringStats =
-    { title = "Keyring"
-    , icon = Icon.keyring
-    , price = Coin.create 10000
-    , unlockLevel = 1
-    , reward =
-        ShopItemIntervalMod
-            (List.map (\choreKind -> { kind = choreKind, percentChange = 0.1 }) Chore.allKinds)
-    , description = "+10% faster at All Chores"
-    , getter = .keyring
-    , setter = \owned ownedItems -> { ownedItems | keyring = owned }
-    }
-
-
-readingGlassesStats =
-    { title = "Reading Glasses"
-    , icon = Icon.readingGlasses
-    , price = Coin.create 3000
-    , unlockLevel = 55
-    , reward =
-        ShopItemMod
-            [ Event.choresMxpBuff 0.2
-                |> Event.withSource Event.ShopItem
-                |> Event.modWithTags
-                    [ Event.ChoreTag OrganizePotionIngredients
-                    ]
-            , Event.choresMxpBuff 0.2
-                |> Event.withSource Event.ShopItem
-                |> Event.modWithTags
-                    [ Event.ChoreTag OrganizeSpellBooks
-                    ]
-            ]
-    , description = "+20% Mastery XP from Organize Potion Ingredients and Organize Spell Books"
-    , getter = .readingGlasses
-    , setter = \owned ownedItems -> { ownedItems | readingGlasses = owned }
-    }
-
-
-oversizedBagStats =
-    { title = "OversizedBag"
-    , icon = Icon.oversizedBag
-    , price = Coin.create 5000
-    , unlockLevel = 65
-    , reward =
-        ShopItemMod
-            [ Event.successBuff 0.75
-                |> Event.withSource Event.ShopItem
-                |> Event.modWithTags
-                    [ Event.ChoreTag RepairInstruments ]
-            ]
-    , description = "Always receive item from Repair Instruments"
-    , getter = .oversizedBag
-    , setter = \owned ownedItems -> { ownedItems | oversizedBag = owned }
-    }
-
-
 getStats : Kind -> Stats
 getStats kind =
     case kind of
         Shovel ->
-            shovelStats
+            { title = "Shovel"
+            , icon = Icon.shovel
+            , price = Coin.create 50
+            , unlockLevel = 1
+            , reward =
+                ShopItemIntervalMod
+                    [ { kind = CleanStables, percentChange = 0.05 }
+                    , { kind = CleanBigBubba, percentChange = 0.05 }
+                    ]
+            , description = "+5% faster at Clean Stables and Clean Big Bubba's Stall"
+            , getter = .shovel
+            , setter = \owned ownedItems -> { ownedItems | shovel = owned }
+            }
 
         BeginnerDualWielding ->
-            beginnerDualWieldingStats
+            { title = "Beginner Dual Wielding"
+            , icon = Icon.book
+            , price = Coin.create 6000
+            , unlockLevel = 35
+            , reward = ShopItemIntervalMod [ { kind = WaterGreenhousePlants, percentChange = 1.0 } ]
+            , description = "+100% faster at Water Greenhouse Plants"
+            , getter = .beginnerDualWielding
+            , setter = \owned ownedItems -> { ownedItems | beginnerDualWielding = owned }
+            }
 
         Keyring ->
-            keyringStats
+            { title = "Keyring"
+            , icon = Icon.keyring
+            , price = Coin.create 10000
+            , unlockLevel = 1
+            , reward =
+                ShopItemIntervalMod
+                    (List.map (\choreKind -> { kind = choreKind, percentChange = 0.1 }) Chore.allKinds)
+            , description = "+10% faster at All Chores"
+            , getter = .keyring
+            , setter = \owned ownedItems -> { ownedItems | keyring = owned }
+            }
 
         ReadingGlasses ->
-            readingGlassesStats
+            { title = "Reading Glasses"
+            , icon = Icon.readingGlasses
+            , price = Coin.create 3000
+            , unlockLevel = 55
+            , reward =
+                ShopItemMod
+                    [ Event.choresMxpBuff 0.2
+                        |> Event.withSource Event.ShopItem
+                        |> Event.modWithTags
+                            [ Event.ChoreTag OrganizePotionIngredients
+                            ]
+                    , Event.choresMxpBuff 0.2
+                        |> Event.withSource Event.ShopItem
+                        |> Event.modWithTags
+                            [ Event.ChoreTag OrganizeSpellBooks
+                            ]
+                    ]
+            , description = "+20% Mastery XP from Organize Potion Ingredients and Organize Spell Books"
+            , getter = .readingGlasses
+            , setter = \owned ownedItems -> { ownedItems | readingGlasses = owned }
+            }
 
         OversizedBag ->
-            oversizedBagStats
+            { title = "OversizedBag"
+            , icon = Icon.oversizedBag
+            , price = Coin.create 5000
+            , unlockLevel = 65
+            , reward =
+                ShopItemMod
+                    [ Event.successBuff 0.75
+                        |> Event.withSource Event.ShopItem
+                        |> Event.modWithTags
+                            [ Event.ChoreTag RepairInstruments ]
+                    ]
+            , description = "Always receive item from Repair Instruments"
+            , getter = .oversizedBag
+            , setter = \owned ownedItems -> { ownedItems | oversizedBag = owned }
+            }
 
 
 type alias OwnedItems =
