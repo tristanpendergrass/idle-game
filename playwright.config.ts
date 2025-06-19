71744e12046c53a571176fa8742af4f1f107f708
Tristan <tristanpendergrass@gmail.com>
2025-06-19 13:21:32 -0700

    Add Playwright e2e test

---
+++ playwright.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  webServer: {
+    command: 'lamdera live',
+    port: 8000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+  testDir: 'e2e',
+});
