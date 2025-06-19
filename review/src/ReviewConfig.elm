e9c9f5e739d4b62679a4a0747f71482e83550e52
Tristan Pendergrass <tristanp@dropbox.com>
2025-06-19 10:42:22 -0700

    Run npx elm-review init

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -11,49 +11,9 @@ when inside the directory containing this file.
 
 -}
 
-import Docs.ReviewAtDocs
-import NoConfusingPrefixOperator
-import NoDebug.Log
-import NoDebug.TodoOrToString
-import NoExposingEverything
-import NoImportingEverything
-import NoMissingTypeAnnotation
-import NoMissingTypeAnnotationInLetIn
-import NoMissingTypeExpose
-import NoPrematureLetComputation
-import NoSimpleLetBody
-import NoUnused.CustomTypeConstructorArgs
-import NoUnused.CustomTypeConstructors
-import NoUnused.Dependencies
-import NoUnused.Exports
-import NoUnused.Parameters
-import NoUnused.Patterns
-import NoUnused.Variables
-import Review.Rule as Rule exposing (Rule)
-import Simplify
+import Review.Rule exposing (Rule)
 
 
 config : List Rule
 config =
-    [ Docs.ReviewAtDocs.rule
-    , NoConfusingPrefixOperator.rule
-    , NoDebug.Log.rule
-    , NoDebug.TodoOrToString.rule
-        |> Rule.ignoreErrorsForDirectories [ "tests/" ]
-    , NoMissingTypeExpose.rule
-    , NoSimpleLetBody.rule
-    , NoPrematureLetComputation.rule
-    , NoUnused.Dependencies.rule
-    , NoUnused.Exports.rule
-    , NoUnused.Parameters.rule
-    , NoUnused.Patterns.rule
-    , Simplify.rule Simplify.defaults
-    -- Rules that came with the default config that I don't want
-    -- , NoExposingEverything.rule
-    -- , NoImportingEverything.rule []
-    -- , NoMissingTypeAnnotation.rule
-    -- , NoMissingTypeAnnotationInLetIn.rule
-    -- , NoUnused.CustomTypeConstructorArgs.rule
-    -- , NoUnused.Variables.rule
-    ]
-        |> List.map (Rule.ignoreErrorsForDirectories ["src/Evergreen/"])
+    []
