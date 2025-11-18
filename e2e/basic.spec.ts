ac3bc14d08029764f2cc60d2958332401181b51e
Tristan Pendergrass <tristanp@dropbox.com>
2025-11-17 21:30:40 -0500

    Add basic e2e spec

--- e2e/basic.spec.ts
+++ e2e/basic.spec.ts
@@ -1,8 +1,17 @@
-import { test } from '@playwright/test';
-import { createAndEnterGame, navigateToTab, startActivity } from './utils';
+import { test, expect } from "@playwright/test";
+import { createAndEnterGame, navigateToTab, startActivity } from "./utils";
 
-test('start Gather Sage', async ({ page }) => {
+test("start Gather Sage", async ({ page }) => {
   await createAndEnterGame(page);
-  await navigateToTab(page, 'Herb Gathering');
-  await startActivity(page, 'Gather Sage');
+  await navigateToTab(page, "Herb Gathering");
+  await startActivity(page, "Gather Sage");
+});
+
+test("switch tab and reload page", async ({ page }) => {
+  await createAndEnterGame(page);
+  await navigateToTab(page, "Herb Gathering");
+  await page.reload();
+  await expect(
+    page.getByRole("navigation").getByText("Herb Gathering")
+  ).toBeVisible();
 });
